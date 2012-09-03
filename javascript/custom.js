933e271f00993b1d76a2fb8e77e921893791599c
darcy w. christ <darcy@1000camels.com>
2012-09-03 10:31:44 +0800

    Fixed issue with no all events firing. I needed to be targeting the submit not click event, as the error was present the first time through.

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,15 +1,17 @@
 YUI().use('event', function(Y) {
     
-    var submit_buttons = Y.all('form.mform input');
+    var submit_forms = Y.all('form.mform');
     
-    submit_buttons.each(function(button) {
-        button.on('click', submit_form);
+    submit_forms.each(function(form) {
+        //console.log('registering form '+form.get('id')+' for event');
+        form.on('submit', submit_form);
     });
-        
+    
+    
+    /**
+     * Alert when there are validation errors in the submitted form
+     */
     function submit_form(e) {
-        
-        // we're canceling the event, so let it go (i.e. #id_cancel)
-        if(e.target.get('id') == 'id_cancel') return true;
         
         if(Y.one('.error')) {
             alert('You have not filled in all the required fields. Please correct them first.');
